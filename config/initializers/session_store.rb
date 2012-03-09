70a76a296b7d5d3aadd9430b9d02059284da3ffd
Gaurav Agarwal <gauravag@thoughtworks.com>
2012-03-09 23:14:12 +0530

    first commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PersonalStockForecaster::Application.config.session_store :cookie_store, key: '_Personal Stock Forecaster_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PersonalStockForecaster::Application.config.session_store :active_record_store
